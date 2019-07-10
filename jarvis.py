3a95eaf00a9e4b04d687320a1aa91a3682bad37b
Geoffrey Jount <geoffrey.jount@epita.fr>
2019-07-10 13:47:09 +0200

    Using word families to analyse command

--- jarvis.py
+++ jarvis.py
@@ -10,6 +10,16 @@ chromium_path = '/snap/bin/chromium'
 priscille_ig = 'https://www.instagram.com/priscillerzr'
 
 
+### DATA
+greetings_words = [
+    'hi',
+    'hello',
+    'greetings',
+    'good evening',
+    'good morning',
+]
+
+
 ### ENGINE
 def Say(message):
     print('>> ' + message)
@@ -20,13 +30,30 @@ def MyCommand():
     return command
 
 
+def Contains(Message, Family):
+    """
+    Message : String
+    Family : String[]
+
+    Return whether Message contains any word from Family or not.
+    """
+    MessageWords = Message.split()
+    for word in MessageWords:
+        if word.lower() in Family:
+            return True
+    return False
+
+
 def CheckCommand(command):
     if 'clean' in command and 'eye' in command:
-        webbrowser.get(chromium_path).open(priscille_ig)
         Say('Sure, let me help.')
+        webbrowser.get(chromium_path).open(priscille_ig)
 
-    elif ('what' in command or 'What') and 'up' in command:
+    elif (('what' in command or 'What' in command) and 'up' in command) or ('how' in command and 'you' in command):
         Say('I\'m fine, what about you?')
+
+    elif Contains(command, greetings_words):
+        Say('Hello!')
 
     elif 'email' in command:
         Say('I was not configured to send mails')
