e5e722ba5d4e9b18e60b5b3b6b222d1996f6b3e3
Geoffrey Jount <geoffrey.jount@epita.fr>
2019-07-10 12:01:02 +0200

    Better format of print

--- jarvis.py
+++ jarvis.py
@@ -12,18 +12,17 @@ priscille_ig = 'https://www.instagram.com/priscillerzr'
 
 ### ENGINE
 def Say(message):
-    print(message)
+    print('>> ' + message)
 
 
 def MyCommand():
     command = input()
-    print('You said: ' + command + '\n')
     return command
 
 
 def CheckCommand(command):
     if 'I need to clean my eyes' in command:
-        webbrowser.get(firefox_path).open(priscille_ig)
+        webbrowser.get(chromium_path).open(priscille_ig)
 
     if 'What\'s up' in command:
         Say('I\'m fine, what about you?')
@@ -31,8 +30,10 @@ def CheckCommand(command):
     if 'email' in command:
         Say('I was not configured to send mails')
 
+    else:
+        Say('I beg your pardon?')
+
 
 ### LIFE LOOP
 while True:
-    Say('I am ready for you command')
     CheckCommand(MyCommand())
