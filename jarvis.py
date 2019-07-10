cf10118328fa4e0a77d87b033d0d62c3db15a3e2
Geoffrey Jount <geoffrey.jount@epita.fr>
2019-07-10 11:42:38 +0200

    Delete audio functions

--- jarvis.py
+++ jarvis.py
@@ -4,76 +4,35 @@ import os
 import webbrowser
 import smtplib
 
-def TalkToMe(audio):
-    print(audio)
-    tts = gTTS(text=audio, lang='en')
-    tts.save('audio.mp3')
-    os.system('mpg123 audio.mp3')
+### SETTINGS
+firefox_path = '/usr/bin/firefox'
+chromium_path = '/snap/bin/chromium'
+priscille_ig = 'https://www.instagram.com/priscillerzr'
 
-# Listen to commands
+
+### ENGINE
+def Say(message):
+    print(message)
+
 
 def MyCommand():
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        print('I am ready for you next command')
-        r.pause_threshold = 1
-        r.adjust_for_ambient_noise(source, duration = 1)
-        audio = r.listen(source)
-
-    try:
-        command = r.recognize_google(audio)
-        print('You said: ' + command + '\n')
-
-    # Loop back to continue listening
-
-    except sr.UnknownValueError:
-        assistant(MyCommand())
-
+    command = input()
+    print('You said: ' + command + '\n')
     return command
 
 
-# If statements for executing commands
-
-def assistant(command):
+def CheckCommand(command):
     if 'I need to clean my eyes' in command:
-        firefox_path = '/usr/bin/firefox'
-        url = 'https://www.instagram.com/priscillerzr'
-        webbrowser.get(chrome_path).open(url)
+        webbrowser.get(firefox_path).open(priscille_ig)
 
-    if 'what\'s up' in command:
-        TalkToMe('I am a computer program. Don\'t bother asking.')
+    if 'What\'s up' in command:
+        Say('I\'m fine, what about you?')
 
     if 'email' in command:
-        TalkToMe('I was not configured to send mails')
+        Say('I was not configured to send mails')
 
-        #TalkToMe('Who os the recipient')
-        #recipient = MyCommand()
 
-#        if 'Priscille' in recipient:
-#            TalkToMe('What should I say')
-#            content = MyCommand()
-#
-#            # Init gmail SMTP
-#            mail = smtplib.SMTP('smtp.gmail.com', 587)
-#
-#            # Identify to server
-#            mail.ehlo()
-#
-             # Encrypt
-#            mail.starttls()
-
-             # Login
-#            mail.login('username', 'password')
-
-            # Send message
-#           mail.sendmail('PERSON NAME', 'emailaddress', content)
-
-            # Close connection
-#           mail.close()
-
-#            TalkToMe('Email sent')
-
-TalkToMe('I am ready for you command')
-
+### LIFE LOOP
 while True:
-    assistant(MyCommand())
+    Say('I am ready for you command')
+    CheckCommand(MyCommand())
